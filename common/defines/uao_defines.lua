c12828c7a72a7127545f81e71f17d52a9f3815bd
Shaun Desmond <shaunusdesmondo@hotmail.com>
2021-08-08 00:45:58 +0200

    Change Production License Defines

--- common/defines/uao_defines.lua
+++ common/defines/uao_defines.lua
@@ -1,4 +1,9 @@
+--- Division Designer Change
 NDefines.NMilitary.MAX_DIVISION_BRIGADE_WIDTH = 5; -- Max width of regiments in division designer.
 NDefines.NMilitary.MAX_DIVISION_BRIGADE_HEIGHT = 3; -- Max height of regiments in division designer.
 NDefines.NMilitary.MAX_DIVISION_SUPPORT_WIDTH = 2; -- Max width of support in division designer.
 NDefines.NMilitary.MAX_DIVISION_SUPPORT_HEIGHT = 4; -- Max height of support in division designer.
+
+-- Production License Base Change
+NDefines.NDiplomacy.LICENSE_ACCEPTANCE_OPINION_FACTOR = 0.3
+NDefines.NDiplomacy.LICENSE_ACCEPTANCE_TECH_DIFFERENCE_BASE = 19
